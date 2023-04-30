e60f0d8c30d248405d6685e428f54a519c8be88b
mikaello <2505178+mikaello@users.noreply.github.com>
2023-04-30 17:06:05 +0000

    feat: generic unmarshall v3

--- pkg/converters/converters.go
+++ pkg/converters/converters.go
@@ -5,29 +5,103 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"reflect"
+	"regexp"
 
 	"github.com/mikaello/go-iof-xml/pkg/iof_v3"
 )
 
-func UnmarshalResultListV3(file string) iof_v3.ResultList {
-	xmlFile, err := os.Open(file)
-	// if we os.Open returns an error then handle it
-	if err != nil {
-		fmt.Println(err)
+func getMainElementName(xml string) string {
+	re := regexp.MustCompile("<([A-Z][a-zA-Z]+)")
+	match := re.FindStringSubmatch(xml)
+	if match != nil {
+		return match[1]
 	}
-	defer xmlFile.Close()
+	return ""
+}
 
-	// read our opened xmlFile as a byte array.
-	byteValue, _ := ioutil.ReadAll(xmlFile)
+const UTF8_BOM = "\uFEFF"
 
-	// we initialize our Users array
-	var resultList iof_v3.ResultList
-	// we unmarshal our byteArray which contains our
-	// xmlFiles content into 'users' which we defined above
-	xml.Unmarshal(byteValue, &resultList)
+func removeUTF8BOM(s string, mainElement string) string {
+	if len(s) > 0 && s[0:len(UTF8_BOM)] == UTF8_BOM {
+		fmt.Printf("WARNING: removing BOM from XML of type %s\n", mainElement)
+		s = s[len(UTF8_BOM):]
+	}
+	return s
+}
 
-	return resultList
+func cleanAndUnmarshal(dirtyXml string, xmlType interface{}) interface{} {
+	xmlTypeName := reflect.TypeOf(xmlType).Elem().Name()
+	xmlData := removeUTF8BOM(dirtyXml, xmlTypeName)
 
+	fmt.Printf("Unmarshalling %s.\n", xmlTypeName)
+
+	err := xml.Unmarshal([]byte(xmlData), xmlType)
+	if err != nil {
+		fmt.Printf("ERROR V3: Failed to unmarshal XML (%s): %s\n", xmlTypeName, err)
+	}
+
+	return xmlType
+}
+
+func GenericUnmarshalV3Xml(dirtyXml string) interface{} {
+	mainElementName := getMainElementName(dirtyXml)
+	xmlData := removeUTF8BOM(dirtyXml, mainElementName)
+
+	type temp struct {
+		value interface{}
+	}
+
+	actual := temp{}
+
+	switch mainElementName {
+	case "CompetitorList":
+		actual.value = new(iof_v3.CompetitorList)
+	case "OrganisationList":
+		actual.value = new(iof_v3.OrganisationList)
+	case "EventList":
+		actual.value = new(iof_v3.EventList)
+	case "ClassList":
+		actual.value = new(iof_v3.ClassList)
+	case "EntryList":
+		actual.value = new(iof_v3.EntryList)
+	case "CourseData":
+		actual.value = new(iof_v3.CourseData)
+	case "StartList":
+		actual.value = new(iof_v3.StartList)
+	case "ResultList":
+		actual.value = new(iof_v3.ResultList)
+	case "ServiceRequestList":
+		actual.value = new(iof_v3.ServiceRequestList)
+	case "ControlCardList":
+		actual.value = new(iof_v3.ControlCardList)
+	default:
+		fmt.Printf("hei: %s.\n", mainElementName)
+	}
+
+	err := xml.Unmarshal([]byte(xmlData), &actual.value)
+	if err != nil {
+		fmt.Printf("ERROR V3: Failed to unmarshal XML (%s): %s\n", mainElementName, err)
+	}
+
+	return actual.value
+}
+
+func UnmarshalIofV3CompetitorList(xml string) iof_v3.CompetitorList {
+	result := iof_v3.CompetitorList{}
+	cleanAndUnmarshal(xml, &result)
+	return result
+}
+
+func UnmarshalResultListV3(xmlobj []byte) iof_v3.ResultList {
+	result := iof_v3.ResultList{}
+	err := xml.Unmarshal(xmlobj, &result)
+	fmt.Println(err)
+
+	/*
+		cleanAndUnmarshal(xml, &result)
+	*/
+	return result
 }
 
 func main() {
--- pkg/converters/converters_test.go
+++ pkg/converters/converters_test.go
@@ -1,13 +1,47 @@
 package converters
 
 import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
+func TestUnmarshalAllTypes(t *testing.T) {
+	dirPath := "../../test/v3-examples"
+	entries, err := os.ReadDir(dirPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, e := range entries {
+		file := readFile(filepath.Join(dirPath, e.Name()))
+		_ = GenericUnmarshalV3Xml(string(file))
+	}
+}
+
 func TestUnmarshalResultList(t *testing.T) {
-	resultList := UnmarshalResultListV3("../../ResultList2.xml")
+	file := readFile("../../test/v3-examples/ResultList4.xml")
+	resultList := UnmarshalResultListV3(file)
 
 	if resultList.Event.Name != "Example event" {
 		t.Fatalf("Expected result list event name to be 'Example Event', found %s", resultList.Event.Name)
 	}
+}
+
+func readFile(fileName string) []byte {
+	file, err := os.Open(fileName)
+
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// read our opened xmlFile as a byte array.
+	byteValue, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	return byteValue
 }
